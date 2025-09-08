4573557545d23400144a7799344839f0cea5a663
Van Nguyen <thegoleffect@gmail.com>
2025-09-08 12:44:23 -0700

    Add src/main/java/com/example/Application.java

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+// TODO: Add request logging middleware [ttt.todo.middleware.logging]
